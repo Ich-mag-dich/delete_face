19bce71ba61b65f2c5d9d78b376c047e34215113
Ich-mag-dich <san02@kakao.com>
2023-03-27 19:34:34 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use image::{DynamicImage, GrayImage, Rgb};
+use image::{ DynamicImage, GrayImage, Rgb };
 use imageproc::drawing::draw_filled_rect_mut;
 use imageproc::rect::Rect;
-use rustface::{Detector, FaceInfo, ImageData, Rectangle};
+use rustface::{ Detector, FaceInfo, ImageData, Rectangle };
 use std::fs;
-use std::io::{self, Write};
-use std::path::{Path, PathBuf};
-use std::time::{Duration, Instant};
+use std::io::{ self, Write };
+use std::path::{ Path, PathBuf };
 
 fn main() {
     let line = "------------------------------------------";
@@ -29,23 +28,36 @@ fn main() {
         .collect::<Vec<String>>();
     let file_count = files.len();
     println!("{}\nimage count: {}", line, file_count);
-    let mut detector: Box<dyn Detector> =
-        match rustface::create_detector("./model/seeta_fd_frontal_v1.0.bin") {
-            Ok(detector) => detector,
-            Err(error) => {
-                println!("Failed to create detector: {}", error.to_string());
-                std::process::exit(1)
-            }
-        };
+    let mut detector: Box<dyn Detector> = match
+        rustface::create_detector("./model/seeta_fd_frontal_v1.0.bin")
+    {
+        Ok(detector) => detector,
+        Err(error) => {
+            println!("Failed to create detector: {}", error.to_string());
+            std::process::exit(1)
+        }
+    };
+    let stdout = io::stdout();
     detector.set_min_face_size(20);
     detector.set_max_face_size(500);
     detector.set_score_thresh(0.8); // default: 2.0
-    detector.set_pyramid_scale_factor(0.9); // max: 0.99
+    detector.set_pyramid_scale_factor(0.5); // max: 0.99
     detector.set_slide_window_step(4, 4);
+    let mut before_file_name = String::from("");
     for (count, file) in files.iter().enumerate() {
+        let processing = ((count + 1) as f64) / (files.len() as f64);
         let file_path = file.replace("\"", "");
-        println!("\n{}/{}\n{}", count + 1, file_count, file_path);
-        let image: DynamicImage = image::open(&file_path)
+
+        print_process(
+            &count,
+            &file_count,
+            &file_path.as_str(),
+            &processing,
+            &stdout,
+            &before_file_name
+        );
+        let image: DynamicImage = image
+            ::open(&file_path)
             .expect(format!("Failed to read image: {}", &file_path).as_str());
 
         let mut rgb = image.to_rgb8();
@@ -57,42 +69,91 @@ fn main() {
 
             draw_filled_rect_mut(&mut rgb, rect, Rgb([255, 255, 255]));
         }
-        let output_file: PathBuf = PathBuf::from(format!(
-            "./output/{}DF_{}",
-            input_path.replace("input/", ""),
-            file.as_str()
-                .replace(&input_path.replace("input/", ""), "")
-                .replace("input/", "")
-        ));
+
+        let output_file: PathBuf = PathBuf::from(
+            format!("{}", file.as_str().replace("input", "output"))
+        );
+
         match rgb.save(&output_file) {
-            Ok(_) => println!("Saved result to {}", output_file.display()),
+            Ok(_) => {
+                print_process(
+                    &count,
+                    &file_count,
+                    &output_file.display().to_string(),
+                    &processing,
+                    &stdout,
+                    &before_file_name
+                );
+            }
             Err(message) => {
-                let create_dir_name: PathBuf =
-                    PathBuf::from(format!("./output/{}", input_path.replace("input/", "")));
+                let create_dir_name: PathBuf = PathBuf::from(
+                    format!("./output/{}", input_path.replace("input/", ""))
+                );
                 fs::create_dir_all(&create_dir_name).expect("Failed to create directory");
                 match rgb.save(&output_file) {
-                    Ok(_) => println!("Saved result to {}", output_file.display()),
+                    Ok(_) => {
+                        print_process(
+                            &count,
+                            &file_count,
+                            &output_file.display().to_string(),
+                            &processing,
+                            &stdout,
+                            &before_file_name
+                        );
+                    }
                     Err(message2) => println!("Can't save file, {}, {}", message, message2),
                 }
             }
         }
+        before_file_name = format!(
+            "   {} {} {}% {}     ",
+            count + 1,
+            file_count,
+            processing * 100.0,
+            output_file.to_str().unwrap()
+        );
     }
-    println!("\n{}\nDone!", line);
+    println!("\n\n{}\nDone!", line);
 }
 
+fn print_process(
+    count: &usize,
+    file_count: &usize,
+    file_path: &str,
+    processing: &f64,
+    mut stdout: &io::Stdout,
+    before_file_name: &String
+) {
+    let bar = "█".repeat((processing * 20.0).ceil() as usize);
+    let bar2 = " ".repeat(20 - ((processing * 20.0).ceil() as usize));
+    let string_len = before_file_name.len() + bar.len() + bar2.len();
+    if string_len > 1 {
+        print!("\r");
+        for _ in 1..=string_len {
+            print!(" ");
+        }
+        stdout.flush().unwrap();
+    }
+    print!(
+        "\r [{}{}] {}/{} {:.2}% {} ",
+        bar,
+        bar2,
+        count + 1,
+        file_count,
+        processing * 100.0,
+        file_path
+    );
+    stdout.flush().unwrap();
+}
 fn detect_faces(detector: &mut dyn Detector, gray: &GrayImage) -> Vec<FaceInfo> {
     let (width, height): (u32, u32) = gray.dimensions();
     let mut image: ImageData = ImageData::new(gray, width, height);
-    let now: Instant = Instant::now();
+
     let faces: Vec<FaceInfo> = detector.detect(&mut image);
-    println!(
-        "Found {} faces in {} ms",
-        faces.len(),
-        get_millis(now.elapsed())
-    );
+
     faces
 }
 
-fn get_millis(duration: Duration) -> u64 {
-    duration.as_secs() * 1000u64 + u64::from(duration.subsec_nanos() / 1_000_000)
-}
+// fn get_millis(duration: Duration) -> u64 {
+//     duration.as_secs() * 1000u64 + u64::from(duration.subsec_nanos() / 1_000_000)
+// }
